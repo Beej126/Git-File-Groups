12a73aebe586aa14913f84e795f427f79e68a5c5
Beej <6301228+Beej126@users.noreply.github.com>
2026-03-18 17:40:49 -0700

    chore(release): v0.2.5

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -14,7 +14,6 @@ function bumpPatch(v) {
 try {
   const root = path.join(__dirname, '..');
   const pkgPath = path.join(root, 'package.json');
-  const pkgcPath = path.join(root, 'package.jsonc');
 
   const pkgText = fs.readFileSync(pkgPath, 'utf8');
   const pkg = JSON.parse(pkgText);
@@ -25,12 +24,6 @@ try {
   pkg.version = newVersion;
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n', 'utf8');
 
-  // update package.jsonc by replacing the first occurrence of the version value
-  if (fs.existsSync(pkgcPath)) {
-    let pkgcText = fs.readFileSync(pkgcPath, 'utf8');
-    pkgcText = pkgcText.replace(/("version"\s*:\s*")([^"]+)(")/, `$1${newVersion}$3`);
-    fs.writeFileSync(pkgcPath, pkgcText, 'utf8');
-  }
 
   // Output results for the workflow to capture
   console.log(`VERSION=${newVersion}`);
